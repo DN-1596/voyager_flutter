10f152f0ccc5fdb55fe8e85a9c1d6203e8f34150
DN-1596 <devanshunema96@gmail.com>
2023-04-10 15:05:42 +0530

    - dart publish related changes

--- lib/components/polygon_builder.dart
+++ lib/components/polygon_builder.dart
@@ -3,7 +3,6 @@
 
 
 import 'package:dart_jts/dart_jts.dart' as jts;
-import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'index.dart';
--- lib/components/polyline_builder.dart
+++ lib/components/polyline_builder.dart
@@ -1,7 +1,6 @@
 import 'dart:developer';
 
 import 'package:dart_jts/dart_jts.dart' as jts;
-import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'index.dart';
